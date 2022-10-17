f15c510049af336ee98274781060e7a45151b2fb
MasternakMateusz <masternak.mateusz1@gmail.com>
2022-10-17 18:52:15 +0200

    change auth by swap email_verified for is_active

--- mixspace/music/backend.py
+++ mixspace/music/backend.py
@@ -9,15 +9,13 @@ class CustomUserModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
         if username is None:
-            username = kwargs.get(
-                UserModel.USERNAME_FIELD,
-                kwargs.get(UserModel.EMAIL_FIELD))
+            username = kwargs.get(UserModel.USERNAME_FIELD, kwargs.get(UserModel.EMAIL_FIELD))
         if username is None or password is None:
             return
         
         try:
             user = UserModel._default_manager.get(
-                Q(username__exact=username) | (Q(email__iexact=username) & Q(email_verified=True))
+                Q(username__exact=username) | (Q(email__iexact=username) & Q(is_active=True))
             )
         except UserModel.DoesNotExist:
             # #20760
