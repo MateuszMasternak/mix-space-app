00c739eab589c8b440c1c84225f3a6f29890f73b
MateuszMasternak <masternak.mateusz1@gmail.com>
2023-02-26 13:02:27 +0100

    update admin.py

--- mixspace/music/admin.py
+++ mixspace/music/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import CustomAbstractUser as User
+from .models import CustomAbstractUser as User, Follow, Track, Like
 
 
 admin.site.register(User)
+admin.site.register(Follow)
+admin.site.register(Track)
+admin.site.register(Like)
