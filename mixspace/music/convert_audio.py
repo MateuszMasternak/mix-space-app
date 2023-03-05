dc5063e7fc29721421abc441d499829e39616a98
MateuszMasternak <masternak.mateusz1@gmail.com>
2023-03-05 21:36:50 +0100

    delete prints

--- mixspace/music/convert_audio.py
+++ mixspace/music/convert_audio.py
@@ -8,11 +8,9 @@ import os
 
 def convert_to_mp3(temp_file, file_extension='mp3', content_type='audio/mpeg', bitrate='192k'):
     temp_file_path = temp_file.temporary_file_path()
-    print(temp_file_path)
     audiofile = AudioSegment.from_wav(temp_file_path)
 
     new_path = temp_file_path[:-3] + file_extension
-    print(new_path)
     audiofile.export(new_path, format=file_extension, bitrate=bitrate)
 
     converted_audiofile = File(file=open(new_path, 'rb'), name=Path(new_path))
