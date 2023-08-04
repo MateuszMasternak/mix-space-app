ddd68ac73475d94db712e34a429e00fcdd898ed9
MateuszMasternak <masternak.mateusz1@gmail.com>
2023-08-04 16:38:08 +0200

    update views tests

--- tests/test_views_login.py
+++ tests/test_views_login.py
@@ -1,67 +1,65 @@
-import pytest
-
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 
+from simple_assert_status_codes import assert_status_codes as simple_assert
+from fixtures import sample_user, second_sample_user
+from fixtures import sample_track_sample_user, sample_track_second_sample_user
 
-@pytest.fixture
-def sample_user(django_user_model):
-    username = 'TestUsername'
-    email = 'testemail@example.com'
-    password = 'TestPassword123!'
-    return django_user_model.objects.create_user(username=username,
-                                                 email=email,
-                                                 password=password)
-
-def test_upload_view(client, sample_user):
-    client.force_login(sample_user)
-    url = reverse('upload')
-    response = client.get(url)
-    assert response.status_code == 200
+# def test_upload_view(client, sample_user):
+#     client.force_login(sample_user)
+#     url = reverse('upload')
+#
+#     response = client.get(url)
+#     assert response.status_code == 200
+#
+#     file = SimpleUploadedFile('file.wav', b'file_content',
+#                               content_type='audio/wav')
+#     response = client.post(url, {'csrftoken': client.cookies['csrftoken'].value, 'file': file}, format='multipart')
+#     assert response.status_code == 200
 
 def test_liked_view(client, sample_user):
     client.force_login(sample_user)
     url = reverse('liked')
-    response = client.get(url)
-    assert response.status_code == 200
+    simple_assert(client, url, get=200)
 
 def test_follow_view(client, sample_user):
     client.force_login(sample_user)
     url = reverse('follow', kwargs={'username': 'test'})
-    response = client.get(url)
-    assert response.status_code == 404
+    simple_assert(client, url, get=404, post=404)
 
-def test_follow_view_with_second_user(client, sample_user, django_user_model):
-    username = 'TestUsername2'
-    email = 'testemail2@example.com'
-    password = 'TestPassword123!'
-    django_user_model.objects.create_user(username=username,
-                                          email=email,
-                                          password=password)
+def test_follow_view_with_second_user(client, sample_user, second_sample_user):
     client.force_login(sample_user)
     url = reverse('follow', kwargs={'username': 'TestUsername2'})
-    response = client.get(url)
-    assert response.status_code == 200
+    simple_assert(client, url, get=200, post=200)
 
 def test_following_view(client, sample_user):
     client.force_login(sample_user)
     url = reverse('following')
-    response = client.get(url)
-    assert response.status_code == 200
+    simple_assert(client, url, get=200)
 
 # def test_avatar_upload_view(client, sample_user):
 #     client.force_login(sample_user)
 #     url = reverse('avatar_upload')
-#     response = client.get(url)
-#     assert response.status_code == 302
-#
-# def test_delete_view(client, sample_user):
-#     client.force_login(sample_user)
-#     url = reverse('delete', kwargs={'pk': 1})
-#     response = client.get(url)
-#     assert response.status_code == 302
+#     assert_status_codes(client, url, get=405, post=200)
+
+def test_delete_view_author_logged_in(client, sample_user,
+                                      sample_track_sample_user):
+    client.force_login(sample_user)
+    url = reverse('delete', kwargs={'pk': 1})
+    simple_assert(client, url, get=405, post=200)
+
+def test_delete_view_other_user_logged_in(client, sample_user,
+                                          sample_track_second_sample_user):
+    client.force_login(sample_user)
+    url = reverse('delete', kwargs={'pk': 2})
+    simple_assert(client, url, get=405, post=401)
+
+def test_delete_view_not_existing_track(client, sample_user):
+    client.force_login(sample_user)
+    url = reverse('delete', kwargs={'pk': 1})
+    simple_assert(client, url, get=405, post=404)
 
 def test_log_out_view(client, sample_user):
     client.force_login(sample_user)
     url = reverse('log_out')
-    response = client.get(url)
-    assert response.status_code == 302
+    simple_assert(client, url, _all=302)
--- tests/test_views_logout.py
+++ tests/test_views_logout.py
@@ -1,61 +1,54 @@
 import pytest
 
 from django.urls import reverse
-
+from simple_assert_status_codes import assert_status_codes
 
 @pytest.mark.django_db
 def test_index_view(client):
     url = reverse('index')
-    response = client.get(url)
-    assert response.status_code == 200
+    assert_status_codes(client, url, get=200)
 
 def test_sign_up_view(client):
     url = reverse('sign_up')
     response = client.get(url)
     assert response.status_code == 200
+    # assert_status_codes(client, url, get=200)
 
 def test_log_in_view(client):
     url = reverse('log_in')
     response = client.get(url)
     assert response.status_code == 200
+    # assert_status_codes(client, url, get=200)
 
 @pytest.mark.django_db
 def test_player_view(client):
     url = reverse('player', kwargs={'pk': 1})
-    response = client.get(url)
-    assert response.status_code == 200
+    assert_status_codes(client, url, get=200)
 
 def test_log_out_view(client):
     url = reverse('log_out')
-    response = client.get(url)
-    assert response.status_code == 302
+    assert_status_codes(client, url, _all=302)
 
 def test_upload_view(client):
     url = reverse('upload')
-    response = client.get(url)
-    assert response.status_code == 302
+    assert_status_codes(client, url, _all=302)
 
 def test_liked_view(client):
     url = reverse('liked')
-    response = client.get(url)
-    assert response.status_code == 302
+    assert_status_codes(client, url, _all=302)
 
 def test_follow_view(client):
     url = reverse('follow', kwargs={'username': 'test'})
-    response = client.get(url)
-    assert response.status_code == 302
+    assert_status_codes(client, url, _all=302)
 
 def test_following_view(client):
     url = reverse('following')
-    response = client.get(url)
-    assert response.status_code == 302
+    assert_status_codes(client, url, _all=302)
 
 def test_avatar_upload_view(client):
     url = reverse('avatar_upload')
-    response = client.get(url)
-    assert response.status_code == 302
+    assert_status_codes(client, url, _all=302)
 
 def test_delete_view(client):
     url = reverse('delete', kwargs={'pk': 1})
-    response = client.get(url)
-    assert response.status_code == 302
+    assert_status_codes(client, url, _all=302)
